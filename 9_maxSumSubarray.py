4e07d188f411bca2286c9eb5c4ac0d7ca3856ec6
Deepak <deepakgoswamip88@gmail.com>
2024-05-06 21:30:43 +0530

    Added max sum subarray in an array

---
+++ 9_maxSumSubarray.py
@@ -0,0 +1,11 @@
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+max_local = max_so_far = arr[0]
+
+for num in arr:
+    max_local = max(num, max_local + num)
+    max_so_far = max(max_local, max_so_far)
+
+print(max_so_far)
+
+# Kadane's algorithm
