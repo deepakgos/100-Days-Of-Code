edced00ef44f5b40bab081fde33cd53110d80789
Deepak <deepakgoswamip88@gmail.com>
2024-05-09 18:42:20 +0530

    Updated max sum array with float('-inf')

--- 9_maxSumSubarray.py
+++ 9_maxSumSubarray.py
@@ -1,6 +1,7 @@
 arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
-max_local = max_so_far = arr[0]
+# max_local = max_so_far = arr[0]
+max_local = max_so_far = float('-inf')
 
 for num in arr:
     max_local = max(num, max_local + num)
